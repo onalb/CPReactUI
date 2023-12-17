b6aa9fbec3513428a8efd5865cb6f43a116bc183
OnalBurak <122232143+OnalBurak@users.noreply.github.com>
2023-12-17 16:31:14 -0500

    Install prettier and fix issues

--- src/services/PhotoService.ts
+++ src/services/PhotoService.ts
@@ -3,11 +3,13 @@ export class PhotoService {
 
     public async getPhotoListFromFolder(folder: string): Promise<any> {
         const response = await fetch('http://localhost:3080/api/photo?folder=' + folder);
+        console.log(response)
         return await response.json();
     }
 
     public async getPhotoFromFolder(folder: string, photoName: string): Promise<any> {
         const response = await fetch('http://localhost:3080/api/photo?folder=' + folder + '&image=' + photoName);
+        console.log(response)
         var data = await response.json();
         return data;
     }
