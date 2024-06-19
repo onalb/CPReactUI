461838feb89092c03c2ae33d8911d9b82e1176c6
OnalBurak <122232143+OnalBurak@users.noreply.github.com>
2024-06-18 22:51:03 -0400

    Content and scroll drags for X and Y axes are finished

--- src/components/ImageZoom.tsx
+++ src/components/ImageZoom.tsx
@@ -1,21 +1,23 @@
-import React, { useEffect, useState, Component } from 'react';
+import React, { useState } from 'react';
 import Draggable from 'react-draggable';
 
 const DraggableComponent = () => {
-  const [position1, setPosition1] = useState({ x: 0, y: 0 });
-  const [position2, setPosition2] = useState({ x: 0, y: 0 });
-  const [contentHeight, setContentHeight] = useState(20000);
+  const [positionYScroll, setPositionYScroll] = useState({ x: 0, y: 0 });
+  const [positionContent, setPositionContent] = useState({ x: 0, y: 0 });
+  const [positionXScroll, setPositionXScroll] = useState({ x: 0, y: 0 });
+  const [contentHeight, setContentHeight] = useState(10000);
+  const [contentWidth, setContentWidth] = useState(3000);
   const [scale, setScale] = useState(1);
   const [originX, setOriginX] = useState(0);
   const [originY, setOriginY] = useState(0);
 
-  const [scrollThumbWidth, setScrollThumbWidth] = useState(100);
+  const [scrollBarThickness, setScrollBarThickness] = useState(50);
   const [windowSize, setWindowSize] = useState({
     width: window.innerWidth,
     height: window.innerHeight,
   });
 
-  const calculateScrollThumHeight = () => {
+  const calculateScrollThumbHeight = () => {
     const scrollThumbHeight = windowSize.height * windowSize.height / contentHeight;
     if (scrollThumbHeight < 50) {
       return 50;
@@ -23,28 +25,35 @@ const DraggableComponent = () => {
     return scrollThumbHeight;
   }
 
-  const [scrollThumbHeight, setScrollThumbHeight] = useState(calculateScrollThumHeight);
+  const calculateScrollThumbWidth = () => {
+    const scrollThumbWidth = windowSize.width * windowSize.width / contentWidth;
+    if (scrollThumbWidth < 50) {
+      return 50;
+    }
+    return scrollThumbWidth;
+  }
 
-  // useEffect(() => {
-  //   function handleResize() {
-  //     setWindowSize({
-  //       width: window.innerWidth,
-  //       height: window.innerHeight,
-  //     });
-  //   }
-
-  //   window.addEventListener('resize', handleResize);
-  //   return () => window.removeEventListener('resize', handleResize);
-  // }, []);
+  const [scrollThumbThicknessY, setScrollThumbThicknessY] = useState(calculateScrollThumbHeight);
+  const [scrollThumbThicknessX, setScrollThumbThicknessX] = useState(calculateScrollThumbWidth);
   
   const handleDragContent = (e: any, data: any) => {
-    setPosition2({ x: 0, y: data.y });
-    setPosition1({ x: 0, y: - data.y * (windowSize.height - scrollThumbHeight) / (contentHeight - windowSize.height) });
+    debugger;
+    setPositionContent({ x: data.x , y: data.y });
+    setPositionYScroll({ x: 0, y: - data.y * (windowSize.height - scrollBarThickness - scrollThumbThicknessY) / (contentHeight - windowSize.height) });
+    setPositionXScroll({ y: 0, x: - data.x * (windowSize.width - scrollBarThickness - scrollThumbThicknessX) / (contentWidth - windowSize.width) });
+
   };
 
-  const handleDragScrollBar = (e: any, data: any) => {
-    setPosition1({ x: 0, y: data.y });
-    setPosition2({ x: 0, y: - data.y * (contentHeight - windowSize.height) / (windowSize.height - scrollThumbHeight) });
+  const handleDragScrollBarY = (e: any, data: any) => {
+    setPositionYScroll({ x: 0, y: data.y });
+    setPositionContent({ x: positionContent.x, y: - data.y * (contentHeight - windowSize.height + scrollBarThickness) / (windowSize.height - scrollThumbThicknessY - scrollBarThickness) });
+    // console.log('p1', position1.y);
+    // console.log('p2', position2.y);
+  };
+
+  const handleDragScrollBarX = (e: any, data: any) => {
+    setPositionXScroll({ y: 0, x: data.x });
+    setPositionContent({ y: positionContent.y, x: - data.x * (contentWidth - windowSize.width) / (windowSize.width - scrollThumbThicknessX - scrollBarThickness) });
     // console.log('p1', position1.y);
     // console.log('p2', position2.y);
   };
@@ -55,14 +64,10 @@ const DraggableComponent = () => {
     event.stopPropagation();
     if (scrollableElement) {
       debugger;
-      // const zoomFactor = 0.1 * event.deltaY / Math.sqrt(event.deltaY ** 2);
-      // // const deltaY = event.deltaY;
-      // const currentZoom = parseFloat(scrollableElement.style.transform?.replace('scale(', '').replace(')', '')) || 1;
-      // const newZoom = currentZoom - zoomFactor;
-      // scrollableElement.style.transform = `scale(${newZoom.toFixed(2)})`;
 
       const x = event.pageX - (scrollableElement.clientWidth / 2);
       const y = event.pageY - (scrollableElement.clientHeight / 2);
+      // if the content is getting out of the screen on the left and top keep it in the scrren
       if (event.deltaY < 0) { 
           view.scaleAt({x, y}, 1.1);
           view.applyTo(scrollableElement);
@@ -70,31 +75,25 @@ const DraggableComponent = () => {
           view.scaleAt({x, y}, 1 / 1.1);
           view.applyTo(scrollableElement);
       }
-      const rect = scrollableElement.getBoundingClientRect();
-      const style = window.getComputedStyle(scrollableElement);
-      const matrix = new DOMMatrix(style.transform);
 
-      if (rect.left < 0) {
-        // Adjust the e value to move the element
-        matrix.e -= rect.left;
-        // Apply the new transform matrix
-        scrollableElement.style.transform = matrix.toString();
-      }
+      //// This is to keep the content in the boundires of the screen on the top and left
+      // const rect = scrollableElement.getBoundingClientRect();
+      // const style = window.getComputedStyle(scrollableElement);
+      // const matrix = new DOMMatrix(style.transform);
 
-      if (rect.top < 0) {
-        // Adjust the e value to move the element
-        matrix.f -= rect.top;
-        // Apply the new transform matrix
-        scrollableElement.style.transform = matrix.toString();
-      }
+      // if (rect.left < 0) {
+      //   // Adjust the e value to move the element
+      //   matrix.e -= rect.left;
+      //   // Apply the new transform matrix
+      //   scrollableElement.style.transform = matrix.toString();
+      // }
 
-      // setOriginX(x);
-      // setOriginY(y);
-      // setScale(newScale);
-
-      // scrollableElement.style.position = 'absolute';
-      // scrollableElement.style.left = `${x}px`;
-      // scrollableElement.style.top = `${y}px`;
+      // if (rect.top < 0) {
+      //   // Adjust the e value to move the element
+      //   matrix.f -= rect.top;
+      //   // Apply the new transform matrix
+      //   scrollableElement.style.transform = matrix.toString();
+      // }
     }
   };
 
@@ -136,9 +135,9 @@ const DraggableComponent = () => {
   return (
     <>
       <div id="scrollable-element" onWheel={handleWheel}>
-        <Draggable axis="y" bounds={{top: -contentHeight + windowSize.height, bottom: 0}} position={position2} onDrag={handleDragContent}>
-          <div style={{width: windowSize.width - scrollThumbWidth, height: contentHeight + 'px', background: 'blue', position: 'absolute', left: '0', fontSize: '15px'}}>
-            <div>01</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>10</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>20</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>30</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>40</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>50-1000px</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>59</div><div>60</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>70</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>80</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>90</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>100-2000px</div>
+        <Draggable axis="y" bounds={{top: -contentHeight + windowSize.height, bottom: 0, left:-contentWidth + windowSize.width, right: 0}} position={positionContent} onDrag={handleDragContent}>
+          <div style={{width: contentWidth - scrollBarThickness, height: contentHeight + 'px', background: 'blue', position: 'absolute', left: '0', fontSize: '15px'}}>
+            <div>012345678910 012345678920 012345678930 012345678940 012345678950 012345678960 012345678970 012345678980 012345678990 012345678910 012345678920 012345678930 012345678940 012345678950 012345678960 012345678970 012345678980 012345678990 012345678910 012345678920 012345678930 012345678940 012345678950 012345678960 012345678970 012345678980 012345678990 012345678910 012345678920 012345678930 012345678940 012345678950 012345678960 012345678970 012345678980 012345678990</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>10</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>20</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>30</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>40</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>50-1000px</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>59</div><div>60</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>70</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>80</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>90</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>100-2000px</div>
             <div>01</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>10</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>20</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>30</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>40</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>50-1000px</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>59</div><div>60</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>70</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>80</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>90</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>100-4000px</div>
             <div>01</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>10</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>20</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>30</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>40</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>50-1000px</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>59</div><div>60</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>70</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>80</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>90</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>100-6000px</div>
             <div>01</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>10</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>20</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>30</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>40</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>50-1000px</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>59</div><div>60</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>70</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>80</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>90</div><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div><div>7</div><div>8</div><div>9</div><div>100-8000px</div>
@@ -151,9 +150,14 @@ const DraggableComponent = () => {
           </div>
         </Draggable>
       </div>
-      <Draggable axis="y" bounds="parent" position={position1} onDrag={handleDragScrollBar}>
-        <div style={{width: '100px', height: scrollThumbHeight + 'px', background: 'red', position: 'absolute', right: '0'}}>
-          Scroll me!
+      <Draggable axis="y" bounds={{top:0, bottom: windowSize.height - scrollThumbThicknessY - scrollBarThickness}} position={positionYScroll} onDrag={handleDragScrollBarY}>
+        <div style={{width: `${scrollBarThickness}px`, height: `${scrollThumbThicknessY}px`, background: 'red', position: 'absolute', right: '0'}}>
+        <div>Y</div>
+        </div>
+      </Draggable>
+      <Draggable  axis="x" bounds={{left:0, right: windowSize.width - scrollThumbThicknessX - scrollBarThickness}} position={positionXScroll} onDrag={handleDragScrollBarX}>
+        <div style={{width: `${scrollThumbThicknessX}px`, height: `${scrollBarThickness}px`, background: 'red', position: 'absolute', bottom: '0'}}>
+          -- X --
         </div>
       </Draggable>
     </>
