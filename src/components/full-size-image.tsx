5c6f3080f350109a148775e65202345e8e029400
Burak Onal <developer.onalb@gmail.com>
2025-05-13 23:20:11 -0400

    Open folder when full size image is double clicked

--- src/components/full-size-image.tsx
+++ src/components/full-size-image.tsx
@@ -1,24 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import '../styles/ImageZoom.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
 import applyMouseAndTouchEvents from './mouse-and-touch-events';
 import { removeTrackedEventListeners } from './tracked-event-handler';
+import { useParams } from 'react-router-dom';
+import { openFolder } from '../services/PhotoService';
 
-const ImageGrid: React.FC = () => {
+const FullSizeImage: React.FC = () => {
+  const { imagePath, imageName, originalImagePath } = useParams<{
+    imagePath: string;
+    imageName: string;
+    originalImagePath: string;
+  }>();
+
   // States
-  const [origin, setOrigin] = useState('0 0'); // Initial transform-origin
+  const [origin, setOrigin] = useState('0 0');
+  const [isLoadingCompletedAtStart, setIsLoadingCompletedAtStart] = useState<boolean>(true);
   const [zoomScale, setZoomScale] = useState(1);
   const [isDragging, setIsDragging] = useState(false);
   const [isZooming, setIsZooming] = useState(false);
   const [isLongTouch, setIsLongTouch] = useState(false);
-  const [isLoadingCompletedAtStart, setIsLoadingCompletedAtStart] = useState<boolean>(true);
 
-  // Functions
   useEffect(() => {
     if (isLoadingCompletedAtStart) {
       const cleanup = applyMouseAndTouchEvents(
-        'main-element',
+        'full-size-image',
         null, 
         1,
         setZoomScale, 
@@ -36,18 +40,40 @@ const ImageGrid: React.FC = () => {
   }, [isLoadingCompletedAtStart]);
 
   return (
-    <>  
-    <div
-      id='main-element'
+    <>
+    {imagePath && originalImagePath && (<div
+      id='full-size-image'
       style={{
+        position: 'fixed',
         transformOrigin: origin,
         transform: 'matrix(1, 0, 0, 1, 0, 0)',
+        top: 0,
+        left: 0,
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        background: 'black',
+        zIndex: 1000,
       }}
     >
-      asdasds
-    </div>
+      <img
+        alt='img'
+        src={imagePath}
+        style={{
+          padding: '25px',
+          margin: '100px',
+          color: 'white',
+          border: '25px solid rgba(128,128,128,0.5)',
+          maxWidth: '75%',
+          maxHeight: '75%',
+        }}
+        onDoubleClick={() => openFolder(originalImagePath)}
+      />
+    </div>)}
     </>
   );
 };
 
-export default ImageGrid;
+export default FullSizeImage;
